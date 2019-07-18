fb95ae8e353369df754f17a508512b1c7a1a2ee2
huseyintasci <huseyintasci@yandex.com>
2019-07-18 00:38:21 -0700

    Added Informations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,101 +2,106 @@
 //           Index Html Pegasus
 // ----------------------------------------------------------------------------------------------------
 
-
-$(document).on("click mouseon", "body", function (e) {
+$(document).on("click mouseon", "body", function(e) {
   var x = e.clientX;
   var y = e.clientY;
   var newposX = x - 60;
   var newposY = y - 60;
-  $("#pegasus").css("transform", "translate3d(" + newposX + "px," + newposY + "px,0px)");
+  $("#pegasus").css(
+    "transform",
+    "translate3d(" + newposX + "px," + newposY + "px,0px)"
+  );
 });
 
 // ----------------------------------------------------------------------------------------------------
-//           War Html 
+//           War Html
 // ----------------------------------------------------------------------------------------------------
 
 // Varibles
 
 var gods, greset;
 
-
 // Game Start
 function Start() {
   gods = resetgods();
   greset = resetgame();
   godrender();
+  
 }
 
 // Gods Varibles
 function resetgods() {
-
   return {
-    "Zeus": {
+    Zeus: {
       name: "ZEUS",
       health: 180,
       fight: 10,
       imageUrl: "assets/images/zeus.png",
       enemyfight: 20
     },
-    "Poseidon": {
+    Poseidon: {
       name: "POSEIDON",
       health: 150,
       fight: 9,
       imageUrl: "assets/images/poseidon.png",
       enemyfight: 5
     },
-    "Hades": {
+    Hades: {
       name: "HADES",
       health: 140,
       fight: 8,
       imageUrl: "assets/images/hades.png",
       enemyfight: 4
     },
-    "Ares": {
+    Ares: {
       name: "ARES",
       health: 13,
       fight: 11,
       imageUrl: "assets/images/ares.png",
       enemyfight: 3
     }
-  }
+  };
 }
-
+// Reset game
 function resetgame() {
   return {
     selectedCharacter: null,
     selectedEnemy: null,
     enemycound: 0,
     fightcound: 0
-  }
+  };
 }
-
 
 // Create Gods
 
 function godsdiv(character, key) {
   var charDiv = $("<div class='character' data-name='" + key + "'>");
   var charName = $("<div class='character-name'>").text(character.name);
-  var charImage = $("<img alt='image' class='character-image'>").attr('src', character.imageUrl);
+  var charImage = $("<img alt='image' class='character-image'>").attr(
+    "src",
+    character.imageUrl
+  );
   var charHealth = $("<div class='character-health'>").text(character.health);
-  charDiv.append(charName).append(charImage).append(charHealth);
+  charDiv
+    .append(charName)
+    .append(charImage)
+    .append(charHealth);
   return charDiv;
 }
-
+// Render God
 
 function godrender() {
   var keys = Object.keys(gods);
   for (var i = 0; i < keys.length; i++) {
-
     var characterKey = keys[i];
     var character = gods[characterKey];
 
     var charDiv = godsdiv(character, characterKey);
     $("#gods").append(charDiv);
-
   }
 }
 
+// Render Enemy
 function renderenemy(selectedCharacterKey) {
   var characterKeys = Object.keys(gods);
   for (var i = 0; i < characterKeys.length; i++) {
@@ -113,12 +118,12 @@ function renderenemy(selectedCharacterKey) {
     }
   }
 }
-$("#sel4").css("display", "none");
-$(document).ready(function () {
-  $("#gods").on("click", ".character", function () {
+
+// Select God
+$(document).ready(function() {
+  $("#gods").on("click", ".character", function() {
     var selectedKey = $(this).attr("data-name");
-    greset.selectedCharacter = gods[selectedKey]
- 
+    greset.selectedCharacter = gods[selectedKey];
 
     $("#selected-god").append(this);
 
@@ -126,25 +131,22 @@ $(document).ready(function () {
 
     $("#select-area").hide();
 
-    greset.enemycound = Object.keys(gods).length - 1
+    greset.enemycound = Object.keys(gods).length - 1;
     enemyselect();
-
-  })
+  });
 
   // Enemy Select
-
   function enemyselect() {
-    $(".enemy").on("click.enemySelect", function () {
-
+    $(".enemy").on("click.enemySelect", function() {
       var enemyKey = $(this).attr("data-name");
-      greset.selectedEnemy = gods[enemyKey]
+      greset.selectedEnemy = gods[enemyKey];
 
       $("#Enemy").append(this);
       $("#fightbtn").show();
       $(".enemy").off("click.enemySelect");
-    })
+    });
   }
-
+// Fight Values
   function fight(fightcound) {
     greset.selectedEnemy.health -= greset.selectedCharacter.fight * fightcound;
   }
@@ -152,28 +154,23 @@ $(document).ready(function () {
   function defend() {
     greset.selectedCharacter.health -= greset.selectedEnemy.enemyfight;
   }
-
-
+// Check Win
   function win() {
     return greset.enemycound === 0;
-
   }
-
-
+// Check Charakter dead
   function dead(character) {
     return character.health <= 0;
   }
-
 
   function fightdone() {
     if (dead(greset.selectedCharacter)) {
       $("#new-game").text("You Lose New Game");
       $("#selected-god").empty();
       $("#new-game").show();
-      return true
-
+      return true;
     } else if (dead(greset.selectedEnemy)) {
-      greset.enemycound--
+      greset.enemycound--;
       $("#Enemy").empty();
 
       if (win()) {
@@ -181,29 +178,19 @@ $(document).ready(function () {
         $("#new-game").show();
       } else {
         alert("Great !Select another enemy");
-        
-       
-        enemyselect();
 
+        enemyselect();
       }
-      return true
+      return true;
     }
 
-    return false
+    return false;
   }
 
-
-  function emptyDivs() {
-    $("#selected-god").empty();
-    $("#Enemy").empty();
-    $("#enemy_fight .enemy").empty();
-    $("#gods").empty();
-    $("#select-area").show();
-  }
-
-  $("#fightbtn").on("click.fight", function () {
-
-    greset.fightcound++
+  
+// Fight Button
+  $("#fightbtn").on("click.fight", function() {
+    greset.fightcound++;
 
     fight(greset.fightcound);
     defend();
@@ -216,9 +203,17 @@ $(document).ready(function () {
     }
   });
 
-  $("#new-game").on("click.reset", function () {
+// Empty divs
+  function emptyDivs() {
+    $("#selected-god").empty();
+    $("#Enemy").empty();
+    $("#enemy_fight .enemy").empty();
+    $("#gods").empty();
+    $("#select-area").show();
+  }
 
-
+  // New Game Button
+  $("#new-game").on("click.reset", function() {
     emptyDivs();
 
     $(this).hide();
@@ -227,6 +222,6 @@ $(document).ready(function () {
     $("#sel4").css("display", "none");
     Start();
   });
-
+// Stert Game
   Start();
 });
